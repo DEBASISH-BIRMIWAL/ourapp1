f724378b9743f6465ba2ec98fd72f2c31d6be991
debasish <debasishbirmiwal1@gmail.com>
2023-02-17 09:24:30 +0530

    final commit

--- culturalconnect/core/urls.py
+++ culturalconnect/core/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    #path('signup',views.signup,name='signup'),
+    path('feed',views.index,name='index'),
+    path('',views.signin,name='signin'),
+    path('signup',views.signup,name='signup'),
+    path('signin',views.signin,name='signin'),
+    path('logout',views.logout,name='logout'),
+    path('profile',views.profile,name='profile'),
+    path('aspect',views.aspect,name='aspect'),
+    
 ]
